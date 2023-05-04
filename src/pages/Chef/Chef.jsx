ed3b344c51523c401101d7d1371f25ae9faca48a
Your Name <Rimon0000>
2023-05-04 19:39:58 +0600

    lazy load added

--- src/pages/Chef/Chef.jsx
+++ src/pages/Chef/Chef.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { FaHandPointRight, FaRegThumbsUp } from 'react-icons/fa';
+import LazyLoad from 'react-lazy-load';
 import { Link } from 'react-router-dom';
+
 
 const Chef = ({chef}) => {
     const {id, chef_picture, chef_name,years_of_experience,num_of_recipes,likes} = chef
     return (
         <div>
             <Card>
+               <LazyLoad>
                <Card.Img variant="top" src={chef_picture} />
+               </LazyLoad>
                <Card.Body>
                  <Card.Title className='fw-bold fs-3'>{chef_name}</Card.Title>
                  <Card.Text className='d-flex fw-semibold pt-2 gap-5'>
